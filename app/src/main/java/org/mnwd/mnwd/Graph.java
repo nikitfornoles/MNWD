fcbed34b27907601d25a9ba6fa977908cf7b6cc8
nikitfornoles <cfornoles@gbox.adnu.edu.ph>
2017-09-18 21:31:51 +0800

    Change Navigation Drawer Graph Icon

--- app/src/main/java/org/mnwd/mnwd/Graph.java
+++ app/src/main/java/org/mnwd/mnwd/Graph.java
@@ -40,6 +40,14 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Locale;
 
+/*
+graph icon: ascendant-bars-graphic
+<div>
+	Icons made by <a href="https://www.flaticon.com/authors/simpleicon" title="SimpleIcon">SimpleIcon</a>
+	from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by
+	<a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a>
+</div>
+*/
 public class Graph extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private DrawerLayout drawer;
     private NavigationView navigationView;
