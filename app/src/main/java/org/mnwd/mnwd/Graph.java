3a6d1073c95c71a4218fcb98758bc688ce76bcf4
nikitfornoles <cfornoles@gbox.adnu.edu.ph>
2017-09-18 15:47:02 +0800

    Remove code for consumption history because it is already merged with bill history

--- app/src/main/java/org/mnwd/mnwd/Graph.java
+++ app/src/main/java/org/mnwd/mnwd/Graph.java
@@ -34,12 +34,10 @@ public class Graph extends AppCompatActivity implements NavigationView.OnNavigat
     private Toolbar toolbar = null;
 
     //content
-    private ListView listView;
     private String JSON_STRING;
     //
 
     //SESSION
-    private String session_accountid;
     private SharedPreferences sharedPreferences;
 
     @Override
@@ -72,89 +70,7 @@ public class Graph extends AppCompatActivity implements NavigationView.OnNavigat
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         ReusableFunctions.changeNavDrawerTitle (navigationView, sharedPreferences);
-
-        //ADD
-        listView = (ListView) findViewById(R.id.listView_consumptionhistory);
-        getJSON();
-        //
     }
-
-    //content
-    private void showConsumptionHistory(){
-        JSONObject jsonObject = null;
-        ArrayList<HashMap<String,String>> list = new ArrayList<HashMap<String, String>>();
-        try {
-            jsonObject = new JSONObject(JSON_STRING);
-            JSONArray result = jsonObject.getJSONArray(Config.TAG_JSON_ARRAY);
-
-            for(int i = 0; i<result.length(); i++){
-                JSONObject jo = result.getJSONObject(i);
-                String readingid = jo.getString(Config.TAG_READING_READINGID);
-                String billingdate = jo.getString(Config.TAG_READING_BILLINGDATE);
-                String consumption = jo.getString(Config.TAG_READING_CONSUMPTION);
-
-                HashMap<String,String> readings = new HashMap<>();
-                readings.put(Config.TAG_READING_READINGID, readingid);
-                readings.put(Config.TAG_READING_BILLINGDATE, billingdate);
-                readings.put(Config.TAG_READING_CONSUMPTION, consumption);
-                list.add(readings);
-            }
-
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-
-        ListAdapter adapter = new SimpleAdapter(
-                Graph.this,
-                list,
-                R.layout.list_graph,
-                new String[]{Config.TAG_READING_READINGID, Config.TAG_READING_BILLINGDATE, Config.TAG_READING_CONSUMPTION},
-                new int[]{R.id.reading_readingid, R.id.reading_billingdate, R.id.reading_consumption}
-        );
-
-        listView.setAdapter(adapter);
-    }
-
-    private void getJSON(){
-        class GetJSON extends AsyncTask<Void,Void,String> {
-
-            @Override
-            protected void onPreExecute() {
-                super.onPreExecute();
-            }
-
-            @Override
-            protected void onPostExecute(String s) {
-                super.onPostExecute(s);
-
-                String conn_success = "connection success~";
-                if (s.contains(conn_success)) {
-                    JSON_STRING = s.replaceAll(conn_success, "");
-                    showConsumptionHistory();
-                }
-                else {
-                    Toast.makeText (Graph.this, s, Toast.LENGTH_LONG).show();
-                }
-            }
-
-            @Override
-            protected String doInBackground(Void... params) {
-                //RETRIEVE SESSION DATA
-                session_accountid = sharedPreferences.getString(Config.SESSION_ACCOUNTID, null);
-
-                //argument for the php script
-                HashMap<String,String> parameter = new HashMap<> ();
-                parameter.put(Config.KEY_CON_ACCOUNTID, session_accountid);
-
-                RequestHandler rh = new RequestHandler();
-                String s = rh.sendPostRequest(Config.URL_GETCONSUMPTIONHISTORY, parameter);
-                return s;
-            }
-        }
-        GetJSON gj = new GetJSON();
-        gj.execute();
-    }
-    //
 
     @Override
     public void onBackPressed() {
