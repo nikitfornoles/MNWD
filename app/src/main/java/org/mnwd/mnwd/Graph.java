536e9561574a64c341273e2ff33a435e765f5562
nikitfornoles <cfornoles@gbox.adnu.edu.ph>
2017-09-18 19:27:29 +0800

    Graph

--- app/src/main/java/org/mnwd/mnwd/Graph.java
+++ app/src/main/java/org/mnwd/mnwd/Graph.java
@@ -16,28 +16,49 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.Spinner;
 import android.widget.Toast;
+
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.Locale;
 
 public class Graph extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private DrawerLayout drawer;
     private NavigationView navigationView;
     private Toolbar toolbar = null;
 
+    private Spinner spinner;
+    private int pos;
+    private double y1, y2;
+    //private Date x1, x2;
+    private double x1, x2;
+    private LineGraphSeries<DataPoint> series1, series2;
+    private GraphView graph1, graph2;
+
     //content
     private String JSON_STRING;
     //
 
     //SESSION
+    private String session_accountid;
     private SharedPreferences sharedPreferences;
 
     @Override
@@ -70,6 +91,131 @@ public class Graph extends AppCompatActivity implements NavigationView.OnNavigat
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         ReusableFunctions.changeNavDrawerTitle (navigationView, sharedPreferences);
+
+        //GRAPH
+        graph1 = (GraphView) findViewById(R.id.idGraphBillAmount);
+        graph2 = (GraphView) findViewById(R.id.idGraphCubicMeterUsed);
+        series1 = new LineGraphSeries<DataPoint>();
+        series2 = new LineGraphSeries<DataPoint>();
+        //
+
+        //content
+        getJSON();
+        //
+
+        //GRAPH
+        spinner = (Spinner) findViewById(R.id.idSpinner);
+        String options [] = {"Bill Amount (Php)", "Cubic Meter Used (CuM)"};
+        ArrayAdapter<String> adapter = new ArrayAdapter <> (this, android.R.layout.simple_spinner_dropdown_item, options);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(
+                new AdapterView.OnItemSelectedListener() {
+                    @Override
+                    public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
+                        pos = position;
+                        showGraph();
+                    }
+
+                    @Override
+                    public void onNothingSelected(AdapterView<?> adapterView) {
+                    }
+                }
+        );
+        //
+    }
+
+    //content
+    private void getBillHistory(){
+        JSONObject jsonObject = null;
+        try {
+            jsonObject = new JSONObject(JSON_STRING);
+            JSONArray result = jsonObject.getJSONArray(Config.TAG_JSON_ARRAY);
+
+            for(int i = 0; i<result.length(); i++){
+                JSONObject jo = result.getJSONObject(i);
+                String billingdate = jo.getString(Config.TAG_READING_BILLINGDATE);
+                String consumption = jo.getString(Config.TAG_READING_CONSUMPTION);
+                String billamount = jo.getString(Config.TAG_READING_BILLAMOUNT);
+
+                /*
+                String pattern = "yyyy-MM-dd";
+                DateFormat df = new SimpleDateFormat(pattern, Locale.ENGLISH);
+                try {
+                    Date date = df.parse(billingdate);
+                    System.out.println(date); // 2010-01-02
+                    x1 = date;
+                    x2 = date;
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+                */
+                x1 = i;
+                x2 = i;
+                y1 = Double.parseDouble(billamount);
+                y2 = Double.parseDouble(consumption);
+
+                series1.appendData(new DataPoint(x1, y1), true, result.length());
+                series2.appendData(new DataPoint(x2, y2), true, result.length());
+            }
+            graph1.addSeries(series1);
+            graph2.addSeries(series2);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void getJSON(){
+        class GetJSON extends AsyncTask<Void,Void,String> {
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+
+                String conn_success = "connection success~";
+                if (s.contains(conn_success)) {
+                    JSON_STRING = s.replaceAll(conn_success, "");
+                    getBillHistory();
+                }
+                else {
+                    Toast.makeText (Graph.this, s, Toast.LENGTH_LONG).show();
+                }
+            }
+
+            @Override
+            protected String doInBackground(Void... params) {
+                //RETRIEVE SESSION DATA
+                session_accountid = sharedPreferences.getString(Config.SESSION_ACCOUNTID, null);
+
+                //argument for the php script
+                HashMap<String,String> parameter = new HashMap<> ();
+                parameter.put(Config.KEY_CON_ACCOUNTID, session_accountid);
+
+                RequestHandler rh = new RequestHandler();
+                String s = rh.sendPostRequest(Config.URL_GETBILLHISTORY, parameter);
+                return s;
+            }
+        }
+        GetJSON gj = new GetJSON();
+        gj.execute();
+    }
+    //
+
+    private void showGraph () {
+        if (pos == 0) {
+            graph1.setVisibility(View.VISIBLE);
+            graph2.setVisibility(View.GONE);
+        }
+        else {
+            graph2.setVisibility(View.VISIBLE);
+            graph1.setVisibility(View.GONE);
+        }
     }
 
     @Override
