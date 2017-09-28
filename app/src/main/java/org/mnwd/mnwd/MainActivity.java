b902611d0d2d5a57c3ddaa9261b9046ec41fc81b
nikitfornoles <cfornoles@gbox.adnu.edu.ph>
2017-09-28 23:18:57 +0800

    Improved Login (UI + input validation + error reporting)

--- app/src/main/java/org/mnwd/mnwd/MainActivity.java
+++ app/src/main/java/org/mnwd/mnwd/MainActivity.java
@@ -4,22 +4,22 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
+    private TextInputEditText txtEmailAd,txtPassword;
     private String result = "";
     private Button loginBtn;
-    private EditText txtEmailAd,txtPassword;
     private TextView txtRegistrationLink;
-    //private TextView txtNavBarTitle;
 
     //SESSION
     private SharedPreferences sharedPreferences;
@@ -29,9 +29,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        txtEmailAd = (EditText) findViewById(R.id.idEditEmailAd);
-        txtPassword = (EditText) findViewById(R.id.idEditPassword);
-        //txtNavBarTitle = (TextView) findViewById(R.id.idTxtNavDrawerTitle);
+        txtEmailAd = (TextInputEditText) findViewById(R.id.idEditEmailAd);
+        txtPassword = (TextInputEditText) findViewById(R.id.idEditPassword);
 
         loginBtn = (Button) findViewById(R.id.idBtnLogin);
         loginBtn.setOnClickListener(new View.OnClickListener() {
@@ -43,7 +42,6 @@ public class MainActivity extends AppCompatActivity {
 
         txtRegistrationLink = (TextView)findViewById(R.id.idRegisterLink);
         txtRegistrationLink.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View v) {
                 Intent startIntent = new Intent(getApplicationContext(), Registration1.class);
@@ -55,86 +53,91 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void signin() {
-        final String email = txtEmailAd.getText().toString().trim();
-        final String password = txtPassword.getText().toString().trim();
-
-        String type="login";
-        class SignIn extends AsyncTask<Void, Void, String> {
-            @Override
-            protected void onPreExecute() {
-                super.onPreExecute();
+        if (TextUtils.isEmpty(txtEmailAd.getText().toString().trim()) || TextUtils.isEmpty(txtPassword.getText().toString().trim())) {
+            if (TextUtils.isEmpty(txtEmailAd.getText().toString().trim())) {
+                txtEmailAd.setError("Email address can't be empty");
             }
+            if (TextUtils.isEmpty(txtPassword.getText().toString().trim())) {
+                txtPassword.setError("Password can't be empty");
+            }
+        }
+        else {
+            final String email = txtEmailAd.getText().toString().trim();
+            final String password = txtPassword.getText().toString().trim();
 
-            @Override
-            protected void onPostExecute(String s) {
-                super.onPostExecute(s);
+            class SignIn extends AsyncTask<Void, Void, String> {
+                @Override
+                protected void onPreExecute() {
+                    super.onPreExecute();
+                }
 
-                String conn_success = "connection success~";
-                if (s.contains(conn_success)) {
-                    String login_status = s.replaceAll(conn_success, "");
-                    String login_success = "login successful!";
-                    if (login_status.contains(login_success)) {
-                        //store session in sys/data/data/<package name>/shared preferences
-                        String info = login_status.replaceAll(login_success, "");
-                        String [] infobits = info.split("~");
-                        String userid = infobits [0];
-                        String total_account = infobits [1];
-                        String total_activated_account = infobits [2];
-                        String accountid = infobits [3];
-                        String accountno = infobits [4];
-                        String firstname = infobits [5];
-                        String lastname = infobits [6];
+                @Override
+                protected void onPostExecute(String s) {
+                    super.onPostExecute(s);
 
-                        editor.putString(Config.SESSION_USERID, userid);
-                        editor.putString(Config.SESSION_TOTALACCOUNT, total_account);
-                        editor.putString(Config.SESSION_TOTALACTIVATEDACCOUNT, total_activated_account);
-                        editor.putString(Config.SESSION_FIRSTNAME, firstname);
-                        editor.putString(Config.SESSION_LASTNAME, lastname);
-                        editor.apply();
+                    String conn_success = "connection success~";
+                    if (s.contains(conn_success)) {
+                        String login_status = s.replaceAll(conn_success, "");
+                        String login_success = "login successful!";
+                        if (login_status.contains(login_success)) {
+                            //store session in sys/data/data/<package name>/shared preferences
+                            String info = login_status.replaceAll(login_success, "");
+                            String [] infobits = info.split("~");
+                            String userid = infobits [0];
+                            String total_account = infobits [1];
+                            String total_activated_account = infobits [2];
+                            String accountid = infobits [3];
+                            String accountno = infobits [4];
+                            String firstname = infobits [5];
+                            String lastname = infobits [6];
 
-                        if (total_activated_account.equals("1")) {
-                            editor.putString(Config.SESSION_ACCOUNTID, accountid);
-                            editor.putString(Config.SESSION_ACCOUNTNO, accountno);
+                            editor.putString(Config.SESSION_USERID, userid);
+                            editor.putString(Config.SESSION_TOTALACCOUNT, total_account);
+                            editor.putString(Config.SESSION_TOTALACTIVATEDACCOUNT, total_activated_account);
+                            editor.putString(Config.SESSION_FIRSTNAME, firstname);
+                            editor.putString(Config.SESSION_LASTNAME, lastname);
                             editor.apply();
 
-                            Toast.makeText (MainActivity.this, login_success, Toast.LENGTH_LONG).show();
-                            Intent startIntent = new Intent(getApplicationContext(), Home.class);
-                            startActivity(startIntent);
+                            if (total_activated_account.equals("1")) {
+                                editor.putString(Config.SESSION_ACCOUNTID, accountid);
+                                editor.putString(Config.SESSION_ACCOUNTNO, accountno);
+                                editor.apply();
+
+                                Toast.makeText (MainActivity.this, login_success, Toast.LENGTH_LONG).show();
+                                Intent startIntent = new Intent(getApplicationContext(), Home.class);
+                                startActivity(startIntent);
+                            }
+                            else {
+                                Intent startIntent = new Intent(getApplicationContext(), ChooseAccount.class);
+                                startActivity(startIntent);
+                            }
                         }
                         else {
-                            Intent startIntent = new Intent(getApplicationContext(), ChooseAccount.class);
-                            startActivity(startIntent);
+                            Toast.makeText (MainActivity.this, login_status, Toast.LENGTH_LONG).show();
                         }
                     }
                     else {
-                        Toast.makeText (MainActivity.this, login_status, Toast.LENGTH_LONG).show();
+                        Toast.makeText (MainActivity.this, s, Toast.LENGTH_LONG).show();
                     }
                 }
-                else {
-                    Toast.makeText (MainActivity.this, s, Toast.LENGTH_LONG).show();
+
+                @Override
+                protected String doInBackground(Void... voids) {
+                    //store session in sys/data/data/<package name>/shared preferences
+                    editor.putString(Config.SESSION_EMAIL, email);
+                    editor.apply();
+
+                    HashMap<String,String> params = new HashMap<> ();
+                    params.put(Config.KEY_CON_EMAIL,email);
+                    params.put(Config.KEY_CON_PASSWORD,password);
+
+                    RequestHandler rh = new RequestHandler();
+                    result = rh.sendPostRequest(Config.URL_LOGIN, params);
+                    return result;
                 }
             }
-
-            @Override
-            protected String doInBackground(Void... voids) {
-                //store session in sys/data/data/<package name>/shared preferences
-                editor.putString(Config.SESSION_EMAIL, email);
-                editor.apply();
-
-                HashMap<String,String> params = new HashMap<> ();
-                params.put(Config.KEY_CON_EMAIL,email);
-                params.put(Config.KEY_CON_PASSWORD,password);
-
-                RequestHandler rh = new RequestHandler();
-                result = rh.sendPostRequest(Config.URL_LOGIN, params);
-                return result;
-            }
+            SignIn s = new SignIn();
+            s.execute();
         }
-        SignIn s = new SignIn();
-        s.execute();
-    }
-
-    public void btnCancelClick (View v) {
-        finish();
     }
 }
