cf20e2ef5aa108d706cef9defb746b4cbcc2cf34
nikitfornoles <cfornoles@gbox.adnu.edu.ph>
2017-10-02 01:44:06 +0800

    redirect to Notification page when notification icon is clicked

--- app/src/main/java/org/mnwd/mnwd/Announcement.java
+++ app/src/main/java/org/mnwd/mnwd/Announcement.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -47,6 +48,15 @@ public class Announcement extends AppCompatActivity implements NavigationView.On
         setContentView(R.layout.activity_announcement);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent startIntent = new Intent(getApplicationContext(), Notification.class);
+                startActivity(startIntent);
+            }
+        });
 
         drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
